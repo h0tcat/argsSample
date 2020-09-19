a719699132e822af8f52ddc6dd916a1f686ab3de
h0tcat <zer0busamx@yahoo.co.jp>
2020-09-19 11:18:13 +0900

    使い方の説明を修正

--- CommandLine.java
+++ CommandLine.java
@@ -9,7 +9,7 @@ public class CommandLine{
     public static void main(String[] args){  //argsがコマンドライン引数を受け取る
         if(args.length!=2){//必ず二個渡さないと使い方が表示される。
             System.out.println("How to use CommandLine.java");
-            System.out.println("java CommandLine.java Hello World");
+            System.out.println("java CommandLine Hello World");
         }else{ //二個引数が渡されたなら
             
             System.out.printf("2個目のコマンドライン引数は %sです。\n",args[1]);
